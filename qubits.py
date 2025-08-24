d3adf094a9f29b15f94a581fdcb491eccdff722e
Quinn-Thompson <loopvertical@gmail.com>
2025-08-23 21:31:09 -0700

    Added argv

--- qubits.py
+++ qubits.py
@@ -2,15 +2,24 @@ from grovers_algorithm import create_quantum_circuit
 from shors_algorithm import entire_shors_algorithm
 from test_shors import test_shors
 from visualize_gates import visualize
+import sys
+
 
 import os
 os.environ['OMP_NUM_THREADS'] = '1'
 
 def main():
-    # create_quantum_circuit()
-    entire_shors_algorithm()
-    # test_shors()
-    # visualize()
+    argv = sys.argv
+    argc = len(sys.argv)
+
+    if "grovers" in argv or "g" in argv:
+        create_quantum_circuit()
+    if "shors" in argv or "s" in argv:
+        entire_shors_algorithm()
+    if "test_s" in argv:
+        test_shors()
+    if "visualize" in argv or "v" in argv:
+        visualize(argv)
     
 if __name__ == "__main__":
     main()
--- visualize_gates.py
+++ visualize_gates.py
@@ -1,53 +1,61 @@
 import qiskit
 import shors_algorithm
 from quantum_visualization.gui_backend.main_backend import QuantumCircuitWindow
+from typing import List
 
-def visualize():
-    # bloch_sphere_window = QuantumCircuitWindow()
-    # shors_algorithm.sum_gate(visualization_window=bloch_sphere_window)
+def visualize(argv: List[str]) -> None:
+    if "sum" in argv:
+        bloch_sphere_window = QuantumCircuitWindow()
+        shors_algorithm.sum_gate(visualization_window=bloch_sphere_window)
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
 
-    
-    # bloch_sphere_window = QuantumCircuitWindow()
-    # shors_algorithm.carry_gate(visualization_window=bloch_sphere_window)
+    if "carry" in argv:
+        bloch_sphere_window = QuantumCircuitWindow()
+        shors_algorithm.carry_gate(visualization_window=bloch_sphere_window)
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
 
-    # bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
-    # shors_algorithm.adder_gate(visualization_window=bloch_sphere_window)
+    if "adder" in argv:
+        bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
+        shors_algorithm.adder_gate(visualization_window=bloch_sphere_window)
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
-    
-    # bloch_sphere_window = QuantumCircuitWindow()
-    # shors_algorithm.modulated_adder_gate(5, visualization_window=bloch_sphere_window)
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
-    
-    # coprime = 5
-    # product_of_primes = 3 * 2
-    
-    # bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
-    # shors_algorithm.controlled_mod_mult_gate(product_of_primes, 3, visualization_window=bloch_sphere_window)
+    if "m_adder" in argv:
+        bloch_sphere_window = QuantumCircuitWindow()
+        shors_algorithm.modulated_adder_gate(5, visualization_window=bloch_sphere_window)
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
-    
-    # bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
-    # shors_algorithm.modular_exponentiation(product_of_primes, coprime, visualization_window=bloch_sphere_window)
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
 
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
-    
-    # bloch_sphere_window = QuantumCircuitWindow()
-    # shors_algorithm.qpe_gate(6, visualization_window=bloch_sphere_window)
-
-    # bloch_sphere_window.animate_circuit()
-    # bloch_sphere_window.app.exec()
-    
         
-    shors_algorithm.visualize_shors()    
+    coprime = 5
+    product_of_primes = 3 * 2
+    
+    if "cmm" in argv:
+        bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
+        shors_algorithm.controlled_mod_mult_gate(product_of_primes, 3, visualization_window=bloch_sphere_window)
+
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
+    
+    if "mod_exp" in argv:
+        bloch_sphere_window = QuantumCircuitWindow(ignore_entanglement=True)
+        shors_algorithm.modular_exponentiation(product_of_primes, coprime, visualization_window=bloch_sphere_window)
+
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
+
+    if "qpe" in argv:
+        bloch_sphere_window = QuantumCircuitWindow()
+        shors_algorithm.qpe_gate(6, visualization_window=bloch_sphere_window)
+
+        bloch_sphere_window.animate_circuit()
+        bloch_sphere_window.app.exec()
+    
+    if "vshor" in argv:
+        shors_algorithm.visualize_shors()    
